57c30e465f50bcc87e3958ec340a69c5cf318e48
Kritsada Rodrit <rodritkritsada@meta.com>
2025-11-14 18:11:55 +0700

    update auth and dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,31 @@
+import { prisma } from "@/lib/prima"
+import Sidebar from "../components/sidebar"
+
+
+async function DashboardPage () {
+    
+    const totalProducts = await prisma.product.count({
+        
+    })
+
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Sidebar currentPath="/dashboard" />
+            <main className="ml-64 p-8">
+                <div className="mb-8">
+                    <div className="flex items-center justify-between">
+                        <div>
+                            <h1 className="text-2xl font-semibold text-shadow-gray-900">
+                                Dashboard
+                            </h1>
+                            <p className="text-sm text-gray-500">
+                                Welcome back!  Here is an overview of your inventory. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
+export default DashboardPage
---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+
+import { PrismaClient } from "@/app/generated/prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const demoUserId = '9f2fd63f-11f3-4377-bceb-ee81bf50df7c';
+
+  // Create sample products
+  await prisma.product.createMany({
+    data: Array.from({ length: 25 }).map((_, i) => ({
+      userId: demoUserId,
+      name: `Product ${i + 1}`,
+      price: (Math.random() * 90 + 10).toFixed(2),
+      quantity: Math.floor(Math.random() * 20),
+      lowStockAt: 5,
+      createAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * (i * 5)),
+    })),
+  });
+
+  console.log("Seed data created successfully!");
+  console.log(`Created 25 products for user ID: ${demoUserId}`);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
